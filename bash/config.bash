2edf2d75efaef2f65ce77b06d19ebe5a3d18b017
Arkadiy Butermanov <arkadiy.butermanov@flatstack.com>
2018-06-30 19:24:52 +0300

    Add completions

--- bash/config.bash
+++ bash/config.bash
@@ -9,9 +9,8 @@ export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 export LANGUAGE=en_US.UTF-8
 
-export VISUAL="atom --wait"
-export EDITOR="$VISUAL"
-export REACT_EDITOR="$VISUAL"
+export EDITOR="subl --wait"
+export REACT_EDITOR="subl"
 export LESS="FRX"
 export RI="--format ansi -T"
 
