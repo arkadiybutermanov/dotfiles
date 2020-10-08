97dc52bf1b6190219c320d1e4d01a9c289ad7222
Arkadiy Butermanov <arkadiy.butermanov@flatstack.com>
2020-10-08 21:45:19 +0200

    Fix nvmrc

--- bash/acl.bash
+++ bash/acl.bash
@@ -1,2 +1,3 @@
 export BOXEN_SOCKET_DIR=/usr/local/var/project-sockets
 export RAILS_NO_DEBUG_ASSETS=1
+export GIT_TOKEN=$(security find-generic-password -a "$USER" -s "github_token" -w)
--- nvm/config.bash
+++ nvm/config.bash
@@ -11,7 +11,7 @@ load-nvmrc() {
   local nvmrc_path="$(nvm_find_nvmrc)"
 
   if [ -n "$nvmrc_path" ]; then
-    local nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
+    local nvmrc_node_version=$(nvm version "$(head -n 1 "${nvmrc_path}")")
 
     if [ "$nvmrc_node_version" = "N/A" ]; then
       nvm install
